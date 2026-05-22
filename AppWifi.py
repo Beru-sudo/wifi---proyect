91af59c03c9a68de5dc8fe5d5884c8a985990ec8
Beru - sudo <sasesebastian4@gmail.com>
2026-05-21 20:26:41 -0500

    Actualización de diseño y centrado

--- AppWifi.py
+++ AppWifi.py
@@ -1,18 +1,10 @@
 from flask import Flask, render_template
-import qrcode
 
 app = Flask(__name__)
 
 # DATOS DEL WIFI
 WIFI_NAME = "ECONOFIBRA_ROCIO"
 WIFI_PASSWORD = "##10821082"
-
-# FORMATO WIFI QR
-wifi_data = f"WIFI:T:WPA;S:{WIFI_NAME};P:{WIFI_PASSWORD};;"
-
-# GENERAR QR
-qr = qrcode.make(wifi_data)
-qr.save("static/wifi_qr.png")
 
 # PAGINA PRINCIPAL
 @app.route("/")
